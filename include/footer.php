bfed36306505d354c828e0c6d9bcd46877ecd678
x606x <63947847+x606x@users.noreply.github.com>
2020-04-19 11:00:52 +0300

    v0.1

---
+++ include/footer.php
@@ -0,0 +1,14 @@
+
+
+	<div id="footer">
+		<p>© 2020 MyCafé.com Store. All Rights Reserved.</p>
+		<p> Designed By Fadi(440178159)</p>
+		</div>
+
+	</div>
+
+
+</body>
+
+<!-- Here Ends The HTML file --Fadi -->
+</html>
---
+++ include/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+	<!--Here starts the HTML file - Fadi -->
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta keywords="Coffee, Tea, Drinks" />
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>:: My Café ☕ ::</title>
+
+</head>
+<body>
+	<!--ID for Body-->
+
+	<div id="coffeesite">
+	<div id="fheader">
+	<div id="nav">
+		<nav>
+			<ul>
+			<li>| <a href="index.php"><i class="fas fa-home"></i> HOME</a></li>
+			<li>| <a href="ABOUT.php"><i class="fas fa-address-card"></i> ABOUT US</a></li>
+			<li>| <div class="dropdown">
+					<div class="dropbtn"><a href="MENU.php"><i class="fas fa-th-list"></i> MENU</a></div>
+					<div class="dropdown-content">
+						<a href="products.php">Products</a>
+						<a href="#prices">Prices</a>
+						<a href="#photos">Photos</a>
+					</div>
+				</div>
+			</li>
+			<li>| <a href="CONTACT.php"><i class="fas fa-envelope"></i> CONTACT US</a></li>
+			</ul>
+		</nav>
+	</div>
+		<!--Header Starter-->
+		<div class="left">
+			<a href="index.php">
+				<img src="images/logo1.png" width="75px"; alt="Pictuer of logo">
+			</a>
+		</div>
+		<!--Header LOGO-->
+		<div class="left">
+		<h1>:: My Café ☕::</h1></div>
+
+	</div>
